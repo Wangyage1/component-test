38d0bda0dc8a737f4842f0f10a7ba5ca628e4860
wangyage <smallsun_229@foxmail.com>
2018-01-12 15:15:10 +0800

    删掉css文件

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import  React from 'react';
 import Icon from './Icon.js';
 import Loading from './Loading.js';
-import RadioButton from './RadioButton.js';
 import SearchInput from './SearchInput.js';
 
 export { Icon, Loading, RadioButton, SearchInput };
