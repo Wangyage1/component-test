1c90cd0dbd4cecaa02cd79ec3430b3a3cb01dc0d
wangyage <smallsun_229@foxmail.com>
2018-01-12 13:07:09 +0800

    项目完善与打包

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import  React from 'react';
-import ConfirmModal from './ConfirmModal.js';
 import Icon from './Icon.js';
 import Loading from './Loading.js';
 import RadioButton from './RadioButton.js';
 import SearchInput from './SearchInput.js';
 
-export { ConfirmModal, Icon, Loading, RadioButton, SearchInput };
+export { Icon, Loading, RadioButton, SearchInput };
